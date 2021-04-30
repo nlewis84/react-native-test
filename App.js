aba5da270c1d99689e85e8456ec83e37bb17b383
nlewis84 <n.lewis.perc@gmail.com>
2021-04-30 17:52:25 -0500

    set new background color

--- App.js
+++ App.js
@@ -1,39 +1,51 @@
-import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, FlatList, Pressable, Alert } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
-  const [countriesData, setCountriesData] = useState([])
+  const [countriesData, setCountriesData] = useState([]);
 
   function fetchCountriesData() {
     fetch('https://restcountries.eu/rest/v2/region/africa?fields=name')
       .then((response) => response.json())
       .then((json) => setCountriesData(json))
-      .catch((error) => console.error(error))
+      .catch((error) => console.error(error));
   }
 
-  useEffect(()=> {
+  useEffect(() => {
     fetchCountriesData();
-  })
+  });
 
   return (
-   <FlatList 
-      data={countriesData}
-      contentContainerStyle={styles.container}
-      keyExtractor={item => item.name}
-      renderItem={({item}) => <Text style={styles.text}>{item.name}</Text>}
-   /> 
+    <>
+      <StatusBar style="light" />
+      <FlatList
+        data={countriesData}
+        contentContainerStyle={styles.container}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => (
+          <Text
+            onPress={() => {
+              Alert.alert(`The Capital of ${item.name} is ${item.capital}`);
+            }}
+            style={styles.text}
+          >
+            {item.name}
+          </Text>
+        )}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 30
+    paddingTop: 30,
+    backgroundColor: '#483D8B',
   },
   text: {
-    fontSize: 20,
-    margin: 10
-  },
-  red: {
-    color: 'red',
+    fontSize: 18,
+    margin: 5,
+    color: '#fff',
   },
 });
