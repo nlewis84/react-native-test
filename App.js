0c3beee83325e0418b39d4c0e4310fb0a577e06a
nlewis84 <n.lewis.perc@gmail.com>
2021-04-30 16:14:19 -0500

    updated App.js styles

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.bigBlue}>Hey Katie! Can you see this now?</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +17,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+    alignItems: 'center',
+  },
+  red: {
+    color: 'red',
   },
 });
